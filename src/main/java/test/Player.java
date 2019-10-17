a71b4a5adcd1e7d8c4338355fc715a927153edff
rasarex <rasarex@o2.pl>
2019-10-17 16:50:17 +0200

    Adding name to player

--- src/main/java/test/Player.java
+++ src/main/java/test/Player.java
@@ -2,6 +2,7 @@ package  test;
 import java.util.Random;
 class Player{
     Player(){
+        name = "Blank";
         offset = 1;
         range = 6;
         randMachine = new Random();
@@ -9,7 +10,19 @@ class Player{
     public int guess(){
         return randMachine.nextInt(range) + offset;
     }
+    public void setName(String name){
+        if(name == null )
+            System.out.println("Wrong name set");
+        if(name.length() == 0)
+            System.out.println("Wrong name set");
+        this.name = name;
+    }
+    public  String getName() {
+
+        return name;
+    }
     private Random randMachine;
     public int offset;
     public int range;
+    public String name;
 }
